3a087bb0410a7dbef1af62d092b4a6e0c9f0c954
zyansaber <zyan112491@gmail.com>
2025-12-10 09:03:17 +1100

    Create showOrder.ts

---
+++ src/types/showOrder.ts
@@ -0,0 +1,14 @@
+// src/types/showOrder.ts
+export type ShowOrder = {
+  orderId: string;
+  id?: string;
+  showId: string;
+  date?: string;
+  model?: string;
+  orderType?: string;
+  status?: string;
+  salesperson?: string;
+  chassisNumber?: string;
+  dealerConfirm?: boolean;
+  dealerConfirmAt?: string;
+};
