7d10770fe082c317a1f47152488848e3edc92c4e
zyansaber <zyan112491@gmail.com>
2026-01-29 12:01:37 +1100

    Add dealer overall dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import DealerPortal from "./pages/DealerPortal";
 import AccessRestricted from "./pages/AccessRestricted";
 import InventoryStockPage from "@/pages/InventoryStockPage";
 import DealerDashboard from "./pages/DealerDashboard";
+import DealerOverallDashboard from "./pages/DealerOverallDashboard";
 import UnsignedEmptySlots from "./pages/UnsignedEmptySlots";
 import PasswordLogin from "./pages/PasswordLogin";
 import ProtectedMainRoute from "./components/ProtectedMainRoute";
@@ -110,6 +111,14 @@ const AppShell = () => {
           element={
             <ProtectedDealerRoute>
               <DealerDashboard />
+            </ProtectedDealerRoute>
+          }
+        />
+        <Route
+          path="/dealer/:dealerSlug/overall-dashboard"
+          element={
+            <ProtectedDealerRoute>
+              <DealerOverallDashboard />
             </ProtectedDealerRoute>
           }
         />
