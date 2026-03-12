c6e8c7e1a24b79e71e932c365ffb59a4c133d234
zyansaber <zyan112491@gmail.com>
2026-03-12 23:22:34 +1100

    Revert "Add Smart Scheduling page and navigation to Planning Integration"

--- src/pages/PlanningIntegration.tsx
+++ src/pages/PlanningIntegration.tsx
@@ -14,7 +14,6 @@ import RequsitionPage from "./planningIntegration/RequsitionPage";
 import VehicleSearchPage from "./planningIntegration/VehicleSearchPage";
 import AlertsAndRemindersPage from "./planningIntegration/AlertsAndRemindersPage";
 import AustraliaFactoryCalendarPage from "./planningIntegration/AustraliaFactoryCalendarPage";
-import SmartSchedulingPage from "./planningIntegration/SmartSchedulingPage";
 import { usePlanningData } from "./planningIntegration/usePlanningData";
 import type { Granularity } from "./planningIntegration/types";
 import type { PlanningLang } from "./planningIntegration/i18n";
@@ -122,7 +121,6 @@ export default function PlanningIntegration() {
           <Route path="/requsition" element={<RequsitionPage lang={lang} />} />
           <Route path="/alerts-reminders" element={<AlertsAndRemindersPage rows={data.rows} lang={lang} />} />
           <Route path="/australia-factory-calendar" element={<AustraliaFactoryCalendarPage lang={lang} />} />
-          <Route path="/smart-scheduling" element={<SmartSchedulingPage rows={data.rows} lang={lang} />} />
           <Route path="/vans-in-delay" element={<VansInDelayPage rows={data.rows} lang={lang} />} />
           <Route path="/vehicle-search" element={<VehicleSearchPage rows={data.rows} specByChassis={data.specByChassis} planByChassis={data.planByChassis} lang={lang} />} />
           <Route
